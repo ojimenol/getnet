6695c7f205c83fdab9cf4f48f219732497a5b4eb
ojimenol <oscar@MacBook-Air.local>
2025-11-26 07:40:59 +0100

    Add repo files

--- getnet-cli-nuek/src/test/java/com/santander/getnet/nuek/client/autoconfig/NuekClientAutoConfigurationTest.java
+++ getnet-cli-nuek/src/test/java/com/santander/getnet/nuek/client/autoconfig/NuekClientAutoConfigurationTest.java
@@ -4,10 +4,9 @@ import com.santander.getnet.nuek.client.model.api.NuekApi;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {NuekClientAutoConfiguration.class, JacksonAutoConfiguration.class})
+@SpringBootTest(classes = {NuekClientAutoConfiguration.class})
 class NuekClientAutoConfigurationTest {
 
   @Autowired
